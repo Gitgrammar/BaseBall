33e768684aa880e3424ee364cbaa7c2a6ab10aef
hogehoge <elementrd6@hotmail.com>
2022-05-18 11:12:56 +0900

    打ち返しまで終了

---
+++ Assets/ShootBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ShootBall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float z = 2000;//force strenth
+
+        Rigidbody rd = GetComponent < Rigidbody>();
+
+        rd.AddForce(0, 0, -z);
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
