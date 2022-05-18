307ef9aa03055d350d240181c68a498162d386d9
hogehoge <elementrd6@hotmail.com>
2022-05-18 10:46:32 +0900

    Butter振る

---
+++ Assets/SwingBat.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SwingBat : MonoBehaviour
+{
+    float r = 30;//rotation Speed
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+            transform.Rotate(0, -r, 0);
+        
+    }
+}
