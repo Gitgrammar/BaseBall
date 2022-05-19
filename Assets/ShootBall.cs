905adef0f226e77231a28177ee936cfd99de172c
hogehoge <elementrd6@hotmail.com>
2022-05-20 08:51:53 +0900

    script変更

--- Assets/ShootBall.cs
+++ Assets/ShootBall.cs
@@ -4,20 +4,30 @@ using UnityEngine;
 public class ShootBall : MonoBehaviour
 {
     // Start is called before the first frame update
+    
+        float timer = 0.0f;//force strenth
+        float timeLimit = 1.0f;
+        bool shootSwitch;
+
     void Start()
     {
-        float z = 2000;//force strenth
+        shootSwitch = true;
 
-        Rigidbody rd = GetComponent < Rigidbody>();
-
-        rd.AddForce(0, 0, -z);
-
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if ((timer > timeLimit) & shootSwitch)
+        {
+            float z = 2000;
+        Rigidbody rd = GetComponent<Rigidbody>();
+
+        rd.AddForce(0, 0, -z);
+            timer = 0.0f;
+            shootSwitch = false;
+
+        }
     }
 }
