ad9a79fc6c3eeb951df0ead84f2db91b6fe2d8ea
hogehoge <elementrd6@hotmail.com>
2022-05-26 19:23:19 +0900

    打撃音スクリプト追加

---
+++ Assets/SoundBat.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBat : MonoBehaviour
+{
+    private AudioSource audioSourceBat;
+    public AudioClip seBat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSourceBat = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Ball")
+        {
+            audioSourceBat.PlayOneShot(seBat);
+        }
+    }
+}
