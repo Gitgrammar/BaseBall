79c581f5c9a518ef907c8c8a5dc2701ff3472779
hogehoge <elementrd6@hotmail.com>
2022-05-26 11:01:34 +0900

    particleを追加したがボールの跳ね返り弱いため見れず

---
+++ Assets/WallEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WallEffect : MonoBehaviour
+{
+    private ParticleSystem particle;
+    public int i;
+    void Start()
+    {
+        particle = this.GetComponent<ParticleSystem>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        particle.Play();
+    }
+}
