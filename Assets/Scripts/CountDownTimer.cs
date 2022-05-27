3953793662d9a2cd29da07e4f333e96abd2716b0
hogehoge <elementrd6@hotmail.com>
2022-05-27 13:11:42 +0900

    タイマー追加

---
+++ Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownTimer : MonoBehaviour
+{
+    private int gameTimerInt;
+    private float gameTimerFloat;
+    public Text timeText;
+
+    void Start()
+    {
+        gameTimerFloat = 60.0f;
+        gameTimerInt = (int)gameTimerFloat;
+        timeText.text = "" + gameTimerInt.ToString();
+        
+    }
+
+    void Update()
+    {
+        if (gameTimerFloat>0.0f){
+            gameTimerFloat -= Time.deltaTime;
+            gameTimerInt = (int)gameTimerFloat;
+            timeText.text = "" + gameTimerInt.ToString();
+
+        }
+        if (gameTimerFloat <= 0.0f)
+        {
+            gameTimerInt = 0;
+            timeText.text = "" + gameTimerInt.ToString();
+        }
+        
+    }
+}
