86190e392e56722c065fbf7d195c0e46cd2851cf
hogehoge <elementrd6@hotmail.com>
2022-05-31 11:47:16 +0900

    if文を使うのとscoreをhighscoreに代入で解決

--- Assets/Scripts/AddScore.cs
+++ Assets/Scripts/AddScore.cs
@@ -1,26 +1,32 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
+using System.Collections;
 using UnityEngine.UI;
 
 public class AddScore : MonoBehaviour
 {
-    private int score = 0;
+    public static int score = 0;
     public Text scoreText;
     private int highScore = 0;
     public Text highScoreText;
-    // Start is called before the first frame update
+    private string key = "HIGH SCORE";
+
     void Start()
     {
         score = 0;
         scoreText.text = "0";
-        highScore = PlayerPrefs.GetInt("HighScore");
+        highScore = PlayerPrefs.GetInt(key,0);
         highScoreText.text = "" + highScore.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt(key, highScore);
+            highScoreText.text = "HighScore" + highScore.ToString();
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
